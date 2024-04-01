04d1b0f6ee6dc2944383910e6b4009ff874acf71
Abhijeet Pathare <abhijeetpathare2020@gmail.com>
2024-04-01 16:43:37 +0530

    added eventhandlers and two way data binding

--- blog-app/src/pages/Signup.js
+++ blog-app/src/pages/Signup.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Base from '../components/Base'
 import { Button, Card, CardBody, CardHeader, Col, Container, Form, FormGroup, Input, Label, Row } from 'reactstrap'
 
 function Signup() {
+
+    
+
+    // usestate hook
+    const [data,setData] = useState({
+        name:'',
+        email:'',
+        password:'',
+        about:''
+    })
+
+    const [error,setError] = useState({
+        errors:{},
+        isError:false
+    })
+
+    // handlechange
+    //property makes the change function dynamic
+    //by adding the argument property 
+    const handleChange=(event,property)=>{
+        console.log("fields changed")
+        setData({...data,[property]:event.target.value})
+        
+    }
+
+    // reseting the form data and setting the data to blank on click of reset button
+    const resetData =()=>{
+        setData({ 
+        name:'',
+        email:'',
+        password:'',
+        about:''})
+    }
+
+    //submit the form 
+    const submitForm = (event) =>{
+        event.preventDefault();
+        
+        //data validate
+
+        //call server api for sending the data
+    }
+
+
     return (
         <Base>
             <Container>
+                {JSON.stringify(data)}
                 <Row className='mt-4'>
                     <Col sm={{ size: 6, offset: 3 }}>
                         <Card color='dark' inverse >
@@ -14,7 +59,8 @@ function Signup() {
                             </CardHeader>
                             <CardBody>
                                 {/* Creating form */}
-                                <Form>
+                                {/* Form submission code */}
+                                <Form onSubmit={submitForm}>
                                     <FormGroup>
                                         {/* Name field */}
                                         <Label for="name">
@@ -25,6 +71,8 @@ function Signup() {
                                             name="name"
                                             placeholder="enter name here"
                                             type="name"
+                                            onChange={(e)=>handleChange(e,'name')}
+                                            value={data.name}
                                         />
                                     </FormGroup>
                                     {/* email field */}
@@ -37,6 +85,8 @@ function Signup() {
                                             name="email"
                                             placeholder="email placeholder"
                                             type="email"
+                                            onChange={(e)=>handleChange(e,'email')}
+                                            value={data.email}
                                         />
                                     </FormGroup>
                                     <FormGroup>
@@ -48,6 +98,8 @@ function Signup() {
                                             name="password"
                                             placeholder="password placeholder"
                                             type="password"
+                                            onChange={(e)=>handleChange(e,'password')}
+                                            value={data.password}
                                         />
                                     </FormGroup>
                                     <FormGroup>
@@ -59,11 +111,13 @@ function Signup() {
                                             name="text"
                                             type="textarea"
                                             style={{ height: "250px" }}
+                                            onChange={(e)=>handleChange(e,'about')}
+                                            value={data.about}
                                         />
                                     </FormGroup>
                                     <Container className='text-center'>
                                         <Button type='submit' color='primary'>Register</Button>
-                                        <Button className='ms-2' type='reset' color='secondary'>Register</Button>
+                                        <Button  onClick={resetData} className='ms-2' type='reset' color='secondary'>Reset</Button>
                                     </Container>
                                 </Form>
                             </CardBody>
