9f6df5a019d97163d631962edd8b865a6b6dc6ff
Abhijeet Pathare <abhijeetpathare2020@gmail.com>
2024-04-01 16:01:42 +0530

    added the login component

--- blog-app/src/pages/Login.js
+++ blog-app/src/pages/Login.js
@@ -1,15 +1,53 @@
 import React from 'react'
 import Base from '../components/Base'
+import { Button, Card, CardBody, CardHeader, Col, Container, Form, FormGroup, Input, Label, Row } from 'reactstrap'
 
 function Login() {
-  return (
-  <Base>
-    <div>
-      <h1>This is login page</h1>
-        <p>Welcome to login page</p>
-    </div>
-  </Base>
-  )
+    return (
+        <Base>
+            <Container>
+                <Row >
+                    <Col sm={{ size: 6, offset: 3 }}>
+                        <Card color='info'>
+                            <CardHeader>
+                                <h3> Fill in username and password</h3>
+                            </CardHeader>
+                            <CardBody>
+                            <Form>
+                                {/* email field */}
+                                <FormGroup>
+                                    <Label for="email">
+                                    Email
+                                    </Label>
+                                    <Input 
+                                    placeholder='emailId'
+                                    type="email"
+                                    id='email'
+                                    />
+                                </FormGroup>
+                                {/* password field */}
+                                <FormGroup>
+                                    <Label for="password">
+                                    Email
+                                    </Label>
+                                    <Input 
+                                    placeholder='Password'
+                                    type="password"
+                                    id='password'
+                                    />
+                                </FormGroup>
+                                <Container className='text-center'>
+                                    <Button  color='primary'>Login</Button>
+                                    <Button  color='dark' type='reset' className='ms-4' >Reset</Button>
+                                </Container>
+                            </Form>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </Base>
+    )
 }
 
 export default Login
